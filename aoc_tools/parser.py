4f9a150966002bbaaa14d98ad8e912c156deeda9
Artem Zaets <azaets@outlook.com>
2022-04-18 00:36:29 -0700

    Add parser class for input processing

---
+++ aoc_tools/parser.py
@@ -0,0 +1,38 @@
+import logging
+from typing import List
+
+from aoc_tools.exceptions import FailedToGetChallengeInput
+
+logger = logging.getLogger("AoC-Tools")
+
+
+class Parser:
+    def __init__(self):
+        self.challenge_input = None
+        self.sample_input = None
+
+        self.input_selector = 'challenge'
+
+    def load_inputs(self, challenge_input, sample_input):
+        self.challenge_input = challenge_input
+        self.sample_input = sample_input
+
+    def puzzle_input(self):
+        if self.input_selector == "sample":
+            if not self.sample_input:
+                raise FailedToGetChallengeInput(f"Sample input not found")
+            else:
+                return self.sample_input
+        elif self.input_selector == "challenge":
+            return self.challenge_input
+
+    def set_input(self, sample_input):
+        if sample_input:
+            self.input_selector = "sample"
+        else:
+            self.input_selector = "challenge"
+
+    # Helper Functions
+
+    def str_to_int(self, list_of_str: List[str]) -> List[int]:
+        return list(map(int, list_of_str))
